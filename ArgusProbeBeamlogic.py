199b815220bc0ca3620899d9350dbfa81f77fd2b
Thomas Watteyne <twatteyne@gmail.com>
2016-12-17 01:27:00 +0100

    rewrite of adaptor

--- ArgusProbeBeamlogic.py
+++ ArgusProbeBeamlogic.py
@@ -4,6 +4,8 @@ http://www.beamlogic.com/products/802154-site-analyzer.aspx
 '''
 
 import time
+import struct
+import socket
 import threading
 
 import ArgusVersion
@@ -15,8 +17,14 @@ class PublishThread(threading.Thread):
     '''
     Thread which publishes sniffed frames to the broker.
     '''
+    
+    ZEP_UDP_PORT = 17754
+    
     def __init__(self):
-        pass
+        self.sock = socket.socket(
+            socket.AF_INET,       # IPv4
+            socket.SOCK_DGRAM,    # UDP
+        )
         '''
         RMQ_EXCHANGE = "beamlogic_sniffer"
         RMQ_ROUTING_KEY = "packet"
@@ -47,7 +55,8 @@ class PublishThread(threading.Thread):
     #======================== public ==========================================
     
     def publishFrame(self,frame):
-        raise NotImplementedError()
+        self.sock.sendto(''.join([chr(b) for b in frame]), ('8.8.8.8', self.ZEP_UDP_PORT))
+        #raise NotImplementedError()
     
     #======================== private =========================================
     
@@ -57,9 +66,10 @@ class SnifferThread(threading.Thread):
     Thread which attaches to the sniffer and parses incoming frames.
     '''
     
-    GLOBAL_HEADER_LEN = 24 # 4+2+2+4+4+4+4
-    PACKET_HEADER_LEN = 16 # 4+4+4+4
-    PIPE_SNIFFER      = r'\\.\pipe\analyzer'
+    PCAP_GLOBALHEADER_LEN    = 24 # 4+2+2+4+4+4+4
+    PCAP_PACKETHEADER_LEN    = 16 # 4+4+4+4
+    BEAMLOGIC_HEADER_LEN     = 18 # 8+1+1+4+4
+    PIPE_SNIFFER             = r'\\.\pipe\analyzer'
     
     def __init__(self,publishThread):
         
@@ -71,7 +81,6 @@ class SnifferThread(threading.Thread):
         self.rxBuffer                  = []
         self.doneReceivingGlobalHeader = False
         self.doneReceivingPacketHeader = False
-        self.incl_len                  = None
         
         # start the thread
         threading.Thread.__init__(self)
@@ -84,9 +93,9 @@ class SnifferThread(threading.Thread):
             try:
                 with open(self.PIPE_SNIFFER, 'rb') as sniffer:
                     while True:
-                        b = sniffer.read(1)
+                        b = ord(sniffer.read(1))
                         self._newByte(b)
-            except (IOError,TypeError):
+            except (IOError):
                 print "WARNING: Could not read from pipe at \"{0}\".".format(
                     self.PIPE_SNIFFER
                 )
@@ -101,38 +110,55 @@ class SnifferThread(threading.Thread):
         '''
         Just received a byte from the sniffer
         '''
-        print '{0:02x}'.format(ord(b)),
-        return
         with self.dataLock:
             self.rxBuffer += [b]
             
             # global header
             if   not self.doneReceivingGlobalHeader:
-                if len(self.rxBuffer)==self.GLOBAL_HEADER_LEN:
+                if len(self.rxBuffer)==self.PCAP_GLOBALHEADER_LEN:
                     self.doneReceivingGlobalHeader    = True
                     self.rxBuffer                     = []
             
             # packet header
-            elif not self.doneReceivingGlobalHeader:
-                if len(self.rxBuffer)==self.PACKET_HEADER_LEN:
+            elif not self.doneReceivingPacketHeader:
+                if len(self.rxBuffer)==self.PCAP_PACKETHEADER_LEN:
                     self.doneReceivingPacketHeader    = True
-                    self.incl_len                     = self._extractInclLen(self.rxBuffer)
+                    self.packetHeader                 = self._parsePcapPacketHeader(self.rxBuffer)
+                    assert self.packetHeader['incl_len']==self.packetHeader['orig_len']
                     self.rxBuffer                     = []
             
             # packet data
             else:
-                if len(self.rxBuffer)==self.incl_len:
+                if len(self.rxBuffer)==self.packetHeader['incl_len']:
                     self.doneReceivingPacketHeader    = False
                     self._newFrame(self.rxBuffer)
                     self.rxBuffer                     = []
     
-    def _extractInclLen(self,header):
+    def _parsePcapPacketHeader(self,header):
         '''
-        Extract the incl_len field from a PCAP packet header
-        '''
-        assert len(header)==self.PACKET_HEADER_LEN
+        Parse a PCAP packet header
         
-        raise NotImplementedError()
+        Per https://wiki.wireshark.org/Development/LibpcapFileFormat:
+        
+        typedef struct pcaprec_hdr_s {
+            guint32 ts_sec;         /* timestamp seconds */
+            guint32 ts_usec;        /* timestamp microseconds */
+            guint32 incl_len;       /* number of octets of packet saved in file */
+            guint32 orig_len;       /* actual length of packet */
+        } pcaprec_hdr_t;
+        '''
+        
+        assert len(header)==self.PCAP_PACKETHEADER_LEN
+        
+        returnVal = {}
+        (
+            returnVal['ts_sec'],
+            returnVal['ts_usec'],
+            returnVal['incl_len'],
+            returnVal['orig_len'],
+        ) = struct.unpack('<IIII', ''.join([chr(b) for b in header]))
+        
+        return returnVal
     
     def _newFrame(self,frame):
         '''
@@ -143,13 +169,64 @@ class SnifferThread(threading.Thread):
         frame = self._transformFrame(frame)
         
         # publish frame
-        self.publishThread.publishFrame(self.rxBuffer)
+        self.publishThread.publishFrame(frame)
     
     def _transformFrame(self,frame):
         '''
         Replace BeamLogic header by ZEP header.
         '''
-        raise NotImplementedError()
+        
+        beamlogic  = self._parseBeamlogicHeader(frame[1:1+self.BEAMLOGIC_HEADER_LEN])
+        ieee154    = frame[self.BEAMLOGIC_HEADER_LEN+2:]
+        ieee154[0] = ieee154[0] | 0x40 # fixing PAN ID compression bit (temporary)
+        zep        = self._formatZep(
+            channel     = beamlogic['Channel'],
+            timestamp   = beamlogic['TimeStamp'],
+            length      = len(ieee154),
+        )
+        
+        return zep+ieee154
+    
+    def _parseBeamlogicHeader(self,header):
+        '''
+        Parse a Beamlogic packet header
+        
+        uint64    TimeStamp
+        uint8     Channel
+        uint8     RSSI
+        uint32    GpsLat
+        uint32    GpsLong
+        '''
+        
+        assert len(header)==self.BEAMLOGIC_HEADER_LEN
+        
+        returnVal = {}
+        (
+            returnVal['TimeStamp'],
+            returnVal['Channel'],
+            returnVal['RSSI'],
+            returnVal['GpsLat'],
+            returnVal['GpsLong'],
+        ) = struct.unpack('<QBBII', ''.join([chr(b) for b in header]))
+        
+        return returnVal
+    
+    def _formatZep(self,channel,timestamp,length):
+        return [
+            0x45,0x58,
+            0x02,
+            0x01,
+            channel,
+            0x00,0x01,
+            0x01,
+            0xff,
+        ]+ \
+        [ord(b) for b in struct.pack('>Q',timestamp)]+ \
+        [
+            0x02,0x02,0x02,0x02,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            length,
+        ]
 
 class CliThread(object):
     def __init__(self):
