8d9bd6b9a130c9332a71587134a040152a2f6899
JelenaKovacc <jelenakovac96@gmail.com>
2021-03-31 22:53:25 +0200

    Adding the ZEP header

--- ArgusProbe_Beamlogic.py
+++ ArgusProbe_Beamlogic.py
@@ -235,16 +235,47 @@ class Serial_RxSnifferThread(threading.Thread):
     """
     Thread which attaches to the serial and put frames into queue.
     """
+    # XOFF            is transmitted as [XONXOFF_ESCAPE,           XOFF^XONXOFF_MASK]==[0x12,0x13^0x10]==[0x12,0x03]
+    # XON             is transmitted as [XONXOFF_ESCAPE,            XON^XONXOFF_MASK]==[0x12,0x11^0x10]==[0x12,0x01]
+    # XONXOFF_ESCAPE  is transmitted as [XONXOFF_ESCAPE, XONXOFF_ESCAPE^XONXOFF_MASK]==[0x12,0x12^0x10]==[0x12,0x02]
 
     XOFF                    = 0x13
     XON                     = 0x11
     XONXOFF_ESCAPE          = 0x12
     XONXOFF_MASK            = 0x10
-
-    # XOFF            is transmitted as [XONXOFF_ESCAPE,           XOFF^XONXOFF_MASK]==[0x12,0x13^0x10]==[0x12,0x03]
-    # XON             is transmitted as [XONXOFF_ESCAPE,            XON^XONXOFF_MASK]==[0x12,0x11^0x10]==[0x12,0x01]
-    # XONXOFF_ESCAPE  is transmitted as [XONXOFF_ESCAPE, XONXOFF_ESCAPE^XONXOFF_MASK]==[0x12,0x12^0x10]==[0x12,0x02]
-
+    FCS16TAB = (0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+    0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
+    0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+    0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
+    0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
+    0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+    0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
+    0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
+    0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+    0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
+    0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
+    0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+    0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
+    0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
+    0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+    0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
+    0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
+    0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+    0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
+    0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
+    0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+    0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+    0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
+    0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+    0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
+    0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
+    0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+    0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
+    0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
+    0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+    0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
+    0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
+    )
     def __init__(self, txMqttThread, serialport,baudrate):
 
         # store params
@@ -266,7 +297,6 @@ class Serial_RxSnifferThread(threading.Thread):
         self.receiving               = False
         self.xonxoff_escaping        = False
 
-        self.status                  = ''
         self.HEADER_LENGTH           = 2
 
         # to be assigned, callback
@@ -355,7 +385,7 @@ class Serial_RxSnifferThread(threading.Thread):
 
         except openhdlc.HdlcException as err:
             #log.warning('{}: invalid serial frame: {} {}'.format(self.name, format_string_buf(temp_buf), err))
-            print 'Err'
+            print 'HDLC Exception'
         return valid_frame
 
     def parse_input(self, data):
@@ -373,7 +403,7 @@ class Serial_RxSnifferThread(threading.Thread):
 
         # notify a tuple including source as one hop away nodes elide SRC address as can be inferred from MAC layer
         # header
-        return event_type,data
+        return data
 
     def _newByte(self, b):
         """
@@ -382,7 +412,6 @@ class Serial_RxSnifferThread(threading.Thread):
         if not self.receiving:
                 if self.hdlc_flag and b != self.hdlc.HDLC_FLAG:
                     # start of frame
-                    #print ('Start of HDLC frame..')
                     self.receiving        = True
                     # discard received self.hdlc_flag
                     self.hdlc_flag        = False
@@ -401,7 +430,6 @@ class Serial_RxSnifferThread(threading.Thread):
                     self._rx_buf_add(b)
                 else:
                     # end of frame, received self.hdlc_flag
-                    #print ("End of HDLC frame ..")
                     self.hdlc_flag = True
                     self.receiving = False
                     self._rx_buf_add(b)
@@ -411,7 +439,7 @@ class Serial_RxSnifferThread(threading.Thread):
                         # discard valid frame self.hdlc_flag
                         self.hdlc_flag  = False
                         self._newFrame(self.rxBuffer)
-                        self.rxBuffer           = []
+                        #self.rxBuffer           = []
 
     def _newFrame(self, frame):
         """
@@ -420,10 +448,70 @@ class Serial_RxSnifferThread(threading.Thread):
         #Parse incomming frame
         frame, self.status = self.parse_input(frame)
 
+        # transform frame
+        frame = self._transformFrame(frame)
+
         # publish frame
         #self.txMqttThread.publishFrame(frame)
         pass
 
+    def _transformFrame(self, frame):
+        """
+        Add ZEP header
+        """
+        body      = frame[0:-3]
+        _         = frame[-3:-1]  # crc
+        frequency = frame[-1]
+
+        zep   = self._formatZep(body,frequency)
+        #frame = self._dispatch_mesh_debug_packet(zep)
+        return frame
+
+    def _formatZep (self, body, frequency):
+        # ZEP header
+        zep  = [ord('E'), ord('X')]  # Protocol ID String
+        zep += [0x02]  # Protocol Version
+        zep += [0x01]  # Type
+        zep += [ord(frequency)]  # Channel ID int?
+        zep += [0x00, 0x01]  # Device ID
+        zep += [0x01]  # LQI/CRC mode
+        zep += [0xff]
+        zep += [0x01] * 8  # timestamp
+        zep += [0x02] * 4  # sequence number
+        zep += [0x00] * 10  # reserved
+        zep += [len(body) + 2]  # length
+
+        # mac frame
+        mac = body
+        mac += self.calculate_fcs(mac)
+        return ''.join(map(str,zep)) + mac
+
+    def calculate_fcs(self,rpayload):
+        payload = []
+        a = ''
+        for b in rpayload:
+            payload += [self.byteinverse(ord(b))]
+        crc = 0x0000
+        for b in payload:
+            crc = ((crc << 8) & 0xffff) ^ self.FCS16TAB[((crc >> 8) ^ b) & 0xff]
+        return_val = [
+            self.byteinverse(crc >> 8),
+            self.byteinverse(crc & 0xff),
+        ]
+
+        return a.join(map(str, return_val))
+
+    def byteinverse(self,b):
+    # TODO: speed up through lookup table
+
+        rb = 0
+        for pos in range(8):
+            if b & (1 << pos) != 0: #check this out
+                bitval = 1
+            else:
+                bitval = 0
+            rb |= bitval << (7 - pos)
+        return rb
 
 #########################################################################################
 class TxMqttThread(threading.Thread):
